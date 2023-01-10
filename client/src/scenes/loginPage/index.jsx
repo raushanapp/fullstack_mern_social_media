8e63f39b82e1bb69d833b3f489cfc48d1812e589
raushan kuamr <sraushan890@gmail.com>
2023-01-10 23:06:02 +0530

    login or register part done

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,6 +1,38 @@
-
+import {Box,Typography,useTheme,useMediaQuery} from "@mui/material"
 export const LoginPage = () => {
-    return (
-        <div>Login Page</div>
-    )
+    const theme = useTheme();
+    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+    return <Box>
+        <Box
+            width='100%'
+            backgroundColor={theme.palette.background.alt}
+            p='1rem 6%'
+            textAlign='center'
+        >
+            <Typography
+                fontWeight="bold"
+                fontSize="32px"
+                color="primary"
+            >
+             SocialMedia
+            </Typography>
+        </Box>
+        {/* form  */}
+        <Box
+            width={isNonMobileScreens ? "50%" : "93%"} 
+            p='2rem'
+            m="1rem auto"
+            borderRadius='1.5rem'
+            backgroundColor={theme.palette.background.alt}
+        >
+            <Typography
+                fontWeight='500'
+                variant='h5'
+                sx={{mb:"1.5rem"}}
+            >
+                Welcome to SocialMedia, the Social Media for Sociopath! 
+           </Typography>
+        </Box>
+    </Box>
+    
 }
